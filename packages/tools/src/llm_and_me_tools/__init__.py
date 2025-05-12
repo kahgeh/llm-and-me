830c016f7d6c8712060e183ccbf5454a09847027
kahgeh <kahgeh@hotmail.com>
2025-05-12 21:36:32 +1000

    fix: Not exactly sure what I am fixing, don't think this is required

--- packages/tools/src/llm_and_me_tools/__init__.py
+++ packages/tools/src/llm_and_me_tools/__init__.py
@@ -1,17 +1,17 @@
-from .cortex_mcp_server import main as cortex_main # Added Cortex server main
+from .cortex_mcp_server import main as cortex_main
 from .git_tools_mcp_server import main as git_tools_main
-from .json_to_sqlite import main as json_to_sqlite_main # Added json_to_sqlite tool main
+from .json_to_sqlite import main as json_to_sqlite_main
 from .macos_system_mcp_server import main as macos_main
 from .markdown_mcp_server import main as markdown_main
-from .openapi_mcp_server import main as openapi_main # Added OpenAPI server main
-from .openapi_tools.openapi_to_sqlite import main_cli as openapi_to_sqlite_main_cli # Added OpenAPI to SQLite CLI main
+from .newrelic_mcp_server import main as newrelic_main
+from .openapi_mcp_server import main as openapi_main
 
 __all__ = [
-    "cortex_main", # Added Cortex server main
-    "git_tools_main",
-    "json_to_sqlite_main", # Added json_to_sqlite tool main
-    "macos_main",
-    "markdown_main",
-    "openapi_main", # Added OpenAPI server main
-    "openapi_to_sqlite_main_cli", # Added OpenAPI to SQLite CLI main
+    cortex_main,
+    git_tools_main,
+    json_to_sqlite_main,
+    macos_main,
+    markdown_main,
+    openapi_main,
+    newrelic_main,
 ]
